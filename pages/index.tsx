82b13abc78473ff7813025abc686f46b9f57f43b
Carl <carl.scrivener.89@gmail.com>
2022-07-01 11:37:12 +0100

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,13 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '../components/Link/Link';
-import MHidden from '../components/@MUI-Extended/MHidden';
 
 const Index = function Index() {
   return (
     <main>
       <Container>
         <Grid pt={5} container spacing={4} sx={{ overflow: 'hidden' }}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12}>
             <Box pt={8} px={3}>
               <Box mb={4} display="flex" justifyContent="center">
                 <Box width={150} component="img" src="/static/logo.png" />
@@ -32,16 +31,6 @@ const Index = function Index() {
               </Stack>
             </Box>
           </Grid>
-          <MHidden width="mdDown">
-            <Grid item md={6}>
-              <Box
-                width={800}
-                component="img"
-                src="/static/header-img.jpg"
-                sx={{ position: 'absolute', top: '210px' }}
-              />
-            </Grid>
-          </MHidden>
         </Grid>
       </Container>
     </main>
