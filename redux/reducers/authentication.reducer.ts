df15cdac0b9dfe0f77644454e7bb9ee311a96af6
Carl <carl.scrivener.89@gmail.com>
2022-07-14 16:21:18 +0100

    refactor intial user auth state without need for local storage of user data

--- redux/reducers/authentication.reducer.ts
+++ redux/reducers/authentication.reducer.ts
@@ -1,22 +1,8 @@
 import { accountConstants } from '../constants';
 
-/**
- * If we're running in the browser, validate saved user details, if exists.
- *
- * App will start in 'validating' state on server, so that, initially, preloader
- * is rendered.
- * It is up to the client to detect whether or not a user's details are saved, and
- * to pass these details to the user auth validation.
- */
 const ISSERVER = typeof window === 'undefined';
-let user = {};
-let initialState = { authenticating: true, user: null };
-if (!ISSERVER) {
-  user = JSON.parse(localStorage.getItem('user'));
-  initialState = user ? { authenticating: true, user } : initialState;
-}
 
-function authentication(state: any = initialState, action: any) {
+function authentication(state: any = { authenticating: true }, action: any) {
   switch (action.type) {
     case accountConstants.AUTHENTICATE_REQUEST:
       return {
@@ -46,7 +32,6 @@ function authentication(state: any = initialState, action: any) {
     case accountConstants.LOGIN_SUCCESS:
     case accountConstants.LOGIN_GOOGLE_SUCCESS:
     case accountConstants.LOGIN_FACEBOOK_SUCCESS:
-      if (!ISSERVER) localStorage.setItem('user', JSON.stringify(action.userData));
       return {
         loggedIn: true,
         user: action.userData,
@@ -64,10 +49,7 @@ function authentication(state: any = initialState, action: any) {
     case accountConstants.LOGIN_GOOGLE_FAILURE:
     case accountConstants.LOGIN_FACEBOOK_FAILURE:
     case accountConstants.LOGOUT:
-      if (!ISSERVER) {
-        localStorage.removeItem('user');
-        localStorage.removeItem('demoMode');
-      }
+      if (!ISSERVER) localStorage.removeItem('demoMode');
       return {};
     default:
       return state;
