4b1e0bd24b53ef7dcfdcec29debf7a8a7308f18d
Carl <carl.scrivener.89@gmail.com>
2022-07-25 14:29:53 +0100

    update next.js config vars

--- next.config.js
+++ next.config.js
@@ -5,9 +5,8 @@ module.exports = {
   },
   env: {
     APP_NAME: `BugTrakR`,
-    PL_RESOURCE_URL: 'https://resources.premierleague.com/premierleague',
     FB_APP_ID: '680726796221491',
-    GOOGLE_CLIENT_ID: '57200106798-20rrlje80hsje1m6oddm7tne1q9ucq0e.apps.googleusercontent.com',
+    GOOGLE_CLIENT_ID: '385342326053-pg2tlutgpfksvhbai14m68fh3tia4sur.apps.googleusercontent.com',
     TWITTER_HANDLE: 'bugtrakr',
     GOOGLE_ADSENSE_PUBLISHER_ID: 'ca-pub-3649196559623661',
     GOOGLE_ADSENSE_TEST: 'on',
